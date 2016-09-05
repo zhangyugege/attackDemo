f136c64966da97dc88e6d4529ce1a7801290b0ca
zhangyugege <773772174@qq.com>
2016-09-05 09:25:18 +0800

    add button

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-var wrap = document.querySelector('#wrap');
-wrap.innerHTML = 'c';
+window.onload=function(){
+    var b = document.querySelector('#button');
+    b.onclick = function(){
+        var d = document.createElement('div');
+        d.innerHTML= '<svg xmlns="http://www.w3.org/2000/svg" onclick="alert(document.domain)"></svg>';
+        document.body.appendChild(d);
+    }
+}
